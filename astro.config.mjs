91c8394a8fc572cdaff85d5ffccbe88b3d46153c
Aline <aline@etjaicrie.com>
2026-03-21 14:47:01 +0100

    Switch to hybrid output for better Vercel support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,6 @@ import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://maryan.vercel.app',
-  output: 'static',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-    runtime: 'nodejs22.x',
-  })
+  output: 'hybrid',
+  adapter: vercel()
 });
