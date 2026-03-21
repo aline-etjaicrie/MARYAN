ae3fd34adf37a5aeced199d5b1a3cdb44ebc87df
Aline <aline@etjaicrie.com>
2026-03-21 14:40:50 +0100

    Refonte de la Home, ajout du logo et correction du runtime Node sur Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import vercel from '@astrojs/vercel';
 export default defineConfig({
   site: 'https://maryan.vercel.app',
   output: 'static',
-  adapter: vercel()
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  })
 });
